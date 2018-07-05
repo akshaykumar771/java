e7789d272788e006b4d42d8c5a9e4ee258c69234
akshaykumar771 <akshaykumar.t@razorthink.com>
2018-07-05 15:49:04 +0530

    Updated class CalEngine

--- src/main/java/com/akshay/java/CalEngine.java
+++ src/main/java/com/akshay/java/CalEngine.java
@@ -3,24 +3,28 @@ package com.akshay.java;
 public class CalEngine {
 
     public static void main(String[] args) {
-        double val1 = 100.0d;
-        double val2 = 50.0d;
-        double result;
-        char opCode = 'f';
-
-        if(opCode == 'a')
-            result = val1 + val2;
-        else if (opCode == 's')
-            result = val1 - val2;
-        else if (opCode == 'm')
-            result = val1 * val2;
-        else if (opCode == 'd')
-            result = val1 / val2;
-        else
-            System.out.println("Error - invalid opCode");
-            result = 0.0d;
-
-        System.out.println(result);
+        double[] leftVals = {100.0d, 50.0d, 25.0d, 45.0d};
+        double[] rightVals = {34.0d, 45.0d, 56.0d, 80.0d};
+        char[] opCodes = {'a', 'd', 'm', 's'};
+        double[] results = new double[opCodes.length];
+//        double val1 = 100.0d;
+//        double val2 = 50.0d;
+//        double result;
+//        char opCode = 'f';
+        for (int i = 0; i < opCodes.length; i++) {
+            if (opCodes[i] == 'a')
+                results[i] = leftVals[i] + rightVals[i];
+            else if (opCodes[i] == 's')
+                results[i] = leftVals[i] - rightVals[i];
+            else if (opCodes[i] == 'm')
+                results[i] = leftVals[i] * rightVals[i];
+            else if (opCodes[i] == 'd')
+                results[i] = leftVals[i] / rightVals[i];
+            else {
+                System.out.println("Error - invalid opCode");
+                results[i] = 0.0d;
+            }
+            System.out.println(results[i]);
         }
     }
-
+}
