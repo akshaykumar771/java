7f043b8bf00eae23a48402f5bb16021e953c90ac
akshaykumar771 <akshaykumar.t@razorthink.com>
2018-07-11 12:06:47 +0530

    Updated class CalEngine

--- src/main/java/com/akshay/java/Rectangle.java
+++ src/main/java/com/akshay/java/Rectangle.java
@@ -1,43 +1,67 @@
 package com.akshay.java;
 
 public class Rectangle {
-    int length;
-    int breadth;
-    int area;
+    float length;
+    float breadth;
+    float area;
+    float perimeter;
 
     Rectangle() {
-        length = 0;
-        breadth = 0;
+        length = 1;
+        breadth = 1;
     }
 
-     Rectangle(int i, int i1) {
-        length = i;
-        breadth = i1;
+     public Rectangle(float length, float breadth) throws Exception {
+        if(length<0.0f || breadth>=20.0f){
+            throw new Exception("Length should be greater than 0 and breadth should be less than 20");
+        }
+         this.length = length;
+        this.breadth = breadth;
     }
 
-    public int getLength() {
+
+
+    public float getLength() {
         return length;
     }
 
-    public void setLength(int length) {
-        this.length = length;
+    public void setPermiter(float perimeter) {
+        perimeter = ((getLength()*2) * (getBreadth()*2));
     }
 
-    public int getBreadth() {
+    public float getBreadth() {
         return breadth;
     }
 
-    public void setBreadth(int breadth) {
-        this.breadth = breadth;
+    public void setArea(float area)
+    {
+        area = (getLength()) * (getBreadth());
+    }
+    public float getPerimeter() {
+        return perimeter;
+    }
+
+    public float getArea() {
+        return area;
     }
 
     void areaOfRectangle()
     {
         area = length * breadth;
     }
+    void perimeterOfRectangle() {
+        perimeter = (2 * (length * breadth));
+    }
 
     void displayAreaOfRectangle()
     {
         System.out.println("Area of rectangle is: " + area);
+
     }
+    void displayPerimeterOfRectangle()
+    {
+        System.out.println("Perimeter of rectangle is :" + perimeter);
+    }
+
+
 }
--- src/main/java/com/akshay/java/TestRectangle.java
+++ src/main/java/com/akshay/java/TestRectangle.java
@@ -3,18 +3,12 @@ package com.akshay.java;
 import java.util.Scanner;
 
 public class TestRectangle {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter length");
-        int i = sc.nextInt();
-        System.out.println("enter breadth");
-        int i1 = sc.nextInt();
-        Rectangle rectangle1 = new Rectangle();
-        rectangle1.setLength(i);
-        rectangle1.setBreadth(i1);
-        System.out.println("Length is " + rectangle1.getLength());
-        System.out.println("Breadth is " + rectangle1.getBreadth());
+    public static void main(String[] args) throws Exception {
+        Rectangle rectangle1 = new Rectangle(3.2f,21f);
+        //Rectangle rectangle2 = new Rectangle(20f,21f);
         rectangle1.areaOfRectangle();
         rectangle1.displayAreaOfRectangle();
+        rectangle1.perimeterOfRectangle();
+        rectangle1.displayPerimeterOfRectangle();
     }
 }
